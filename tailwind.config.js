9c5f6ef5e2fd957ebfb776c21f42985a23991475
Ginaldo-Silva <138523497+Ginaldo-Silva@users.noreply.github.com>
2024-08-09 13:00:09 -0300

    Media Query

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '768px',
+      // => @media (min-width: 640px) { ... }
+      'md': '769px',
+      // => @media (min-width: 769px) { ... }
+      'lg': '993px',
+      // => @media (min-width: 993px) { ... }
+    },
   },
   plugins: [],
 }
